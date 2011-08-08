146959bd8eb0fef7bd3dc691f5f55358b928fec8
Eleasah <eleasah.loresco@orangeandbronze.com>
2011-08-08 18:32:30 +0800

    fixed database updated pom.xml
    Signed-off-by: Eleasah <eleasah.loresco@orangeandbronze.com>

--- src/main/java/com/onb/yasah/web/controller/EmployeeController.java
+++ src/main/java/com/onb/yasah/web/controller/EmployeeController.java
@@ -2,8 +2,10 @@ package com.onb.yasah.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.onb.yasah.service.EmployeeService;
+
 
 @Controller
 public class EmployeeController {
@@ -13,6 +15,11 @@ public class EmployeeController {
 	@Autowired
 	public void setEmployeeService(EmployeeService employeeService) {
 		this.employeeService = employeeService;
+	}
+	
+	@RequestMapping()
+	public String showLoginForm() {
+		return "login";
 	}
 	
 	
